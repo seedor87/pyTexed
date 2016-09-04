d0eedff8da33f5fba30cdb1643d9eed72b50b273
Bob <seedor87@students.rowan.edu>
2016-09-04 17:24:19 -0400

    Adding  more fun work on insertion sort and recursion

--- inertion_sort.py
+++ inertion_sort.py
@@ -2,38 +2,50 @@ from random import randint, uniform
 
 
 def insertion_sort(list):
-
-    for j in range(1, len(list)):
-        key = list[j]
+    ret = list
+    for j in range(1, len(ret)):
+        key = ret[j]
         i = j
-        while(i > 0) and list[i-1] > key:
-            list[i] = list[i-1]
+        while(i > 0) and ret[i-1] > key:
+            ret[i] = ret[i-1]
             i = i-1
-        list[i] = key
+        ret[i] = key
+    return ret
 
-    return list
+L_than = lambda A, B: A < B
+G_than = lambda A, B: A > B
+E_to = lambda A, B: A == B or A is B
+G_E_to = lambda A, B: G_than(A, B) or E_to(A, B)
+L_E_to = lambda A, B: L_than(A, B) or E_to(A, B)
 
-def if_in_ascending(list):
-
-    if len(list) <= 1:
+def if_in_order(list, rule=G_E_to):
+    ret = list
+    if len(ret) <= 1:
         return True
-    if list[-1] >= list[-2]:
-        del list[-1]
-        return if_in_ascending(list)
-    else:
-        return False
-
-def if_in_ascending_helper(list):
-    pass
+    if rule(ret[-1], ret[-2]):
+        del ret[-1]
+        return if_in_order(ret)
+    return False
 
 A=[randint(0,9) for p in range(0,9)]
 
-print insertion_sort(A), if_in_ascending(A)
+print insertion_sort(A), if_in_order(A)
 
 B = [uniform(0.0,9.0) for f in range(0,20)]
 
-print insertion_sort(B), if_in_ascending(B)
+print insertion_sort(B), if_in_order(B)
 
-C, D, E = [4, 3], [], [500]
+C, D, E = [3, 4], [], [500]
 
-print if_in_ascending(C), if_in_ascending(D), if_in_ascending(E)
+print if_in_order(C), if_in_order(D), if_in_order(E)
+
+print ""
+
+F, G = [3, 4], [3, 4]
+print if_in_order(F, G_than), if_in_order(G, L_than)
+
+ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n/10%10!=1)*(n%10<4)*n%10::4]) # toy for interpretation
+
+print ""
+
+print [ordinal(n) for n in F]
