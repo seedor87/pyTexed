510fc065ff1945a6d71605523be6a6b139a07217
Bob <seedor87@students.rowan.edu>
2016-09-05 14:37:55 -0400

    added work using lambdas to round

--- inertion_sort.py
+++ inertion_sort.py
@@ -1,16 +1,5 @@
+import sys
 from random import randint, uniform
-
-
-def insertion_sort(list):
-    ret = list
-    for j in range(1, len(ret)):
-        key = ret[j]
-        i = j
-        while(i > 0) and ret[i-1] > key:
-            ret[i] = ret[i-1]
-            i = i-1
-        ret[i] = key
-    return ret
 
 L_than = lambda A, B: A < B
 G_than = lambda A, B: A > B
@@ -18,34 +7,64 @@ E_to = lambda A, B: A == B or A is B
 G_E_to = lambda A, B: G_than(A, B) or E_to(A, B)
 L_E_to = lambda A, B: L_than(A, B) or E_to(A, B)
 
+my_round = lambda L, D: [round(i, D) for i in L ]
+ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n/10%10!=1)*(n%10<4)*n%10::4]) # toy for interpretation
+
+def insertion_sort(list, rule=G_E_to):
+    ret = list
+    for j in range(1, len(ret)):
+        key = ret[j]
+        i = j
+        while(i > 0) and rule(ret[i-1], key):
+            ret[i] = ret[i-1]
+            i = i-1
+        ret[i] = key
+    return ret
+
 def if_in_order(list, rule=G_E_to):
     ret = list
     if len(ret) <= 1:
         return True
     if rule(ret[-1], ret[-2]):
         del ret[-1]
-        return if_in_order(ret)
+        return if_in_order(ret, rule)
     return False
 
-A=[randint(0,9) for p in range(0,9)]
+def test_ins_sort():
 
-print insertion_sort(A), if_in_order(A)
+    ins=[randint(0,9) for x in range(0,9)]
+    out = insertion_sort(ins, rule=L_E_to)
 
-B = [uniform(0.0,9.0) for f in range(0,20)]
+    print out, if_in_order(out, rule=L_E_to)
 
-print insertion_sort(B), if_in_order(B)
+    ins = [uniform(0.0,9.0) for x in range(0,20)]
+    out = insertion_sort(ins)
+    print out, if_in_order(out)
 
-C, D, E = [3, 4], [], [500]
+def test_in_order():
 
-print if_in_order(C), if_in_order(D), if_in_order(E)
+    C, D, E = [3, 4], [], [500]
 
-print ""
+    print if_in_order(C), if_in_order(D), if_in_order(E)
 
-F, G = [3, 4], [3, 4]
-print if_in_order(F, G_than), if_in_order(G, L_than)
+    print ""
 
-ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n/10%10!=1)*(n%10<4)*n%10::4]) # toy for interpretation
+    F, G = [3, 4], [3, 4]
+    print if_in_order(F, G_than), if_in_order(G, L_than)
 
-print ""
+    print ""
 
-print [ordinal(n) for n in F]
+    print [ordinal(n) for n in F]
+
+def test_rounding():
+
+    ins = [uniform(0.0,9.0) for x in range(0,20)]
+    out = my_round(insertion_sort(ins, rule=L_E_to), 4)
+    temp = [i for i in out]
+
+    print out, if_in_order(temp, rule=L_E_to)
+
+if __name__ == '__main__':
+
+    test_rounding()
+    sys.exit(0)
