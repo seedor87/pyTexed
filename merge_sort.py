cf9f8db210f47e2f247854ba7e8e4bb55f69a828
Bob <seedor87@students.rowan.edu>
2016-09-05 14:05:50 -0400

    Adding  more fun work on merge sort

---
+++ merge_sort.py
@@ -0,0 +1,34 @@
+import operator
+from random import randint, uniform
+
+def merge(left,right,compare):
+    result=[]
+    i,j=0,0
+    while i<len(left) and j<len(right):
+        if compare(left[i],right[j]):
+            result.append(left[i])
+            i+=1
+        else:
+            result.append(right[j])
+            j+=1
+    while (i<len(left)):
+        result.append(left[i])
+        i+=1
+    while (j<len(right)):
+        result.append(right[j])
+        j+=1
+    return result
+
+def mergeSort(L,compare=operator.lt):
+    if len(L)<2:
+        return L[:]
+    else:
+        middle =int(len(L)/2)
+        left=mergeSort(L[:middle], compare)
+        right=mergeSort(L[middle:], compare)
+        return merge(left,right,compare)
+
+B = [randint(0.0,30.0) for f in range(0,20)]
+
+res = mergeSort(B)
+print res
